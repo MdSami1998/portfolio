252314dbddb4ac80c027fc918fae384aaa1dc8d6
MdSami1998 <samizaber822@gmail.com>
2022-06-29 23:11:02 -0700

    some changes made

--- src/components/Pages/Projects/Projects.js
+++ src/components/Pages/Projects/Projects.js
@@ -19,14 +19,14 @@ const Projects = () => {
     }, [])
     return (
         <div>
-            <h1 className='text-primary text-center text-3xl uppercase border-b-2 w-48 mx-auto tracking-widest my-5'>Projects</h1>
+            <h1 className='text-primary text-center text-3xl uppercase border-b-2 border-primary w-48 mx-auto tracking-widest my-5'>Projects</h1>
 
             <div className='grid grid-cols-1 md:grid-cols-3 gap-10 px-3 md:px-20 mt-10 w-full'>
                 {projects.map(project => <SingleProject project={project}></SingleProject>)}
             </div>
 
             <div>
-                <h2 className='text-primary text-center text-3xl font-semibold mt-20 border-b-2 w-24 mx-auto tracking-widest'>Skills </h2>
+                <h2 className='text-primary text-center text-3xl font-semibold mt-20 border-b-2 border-primary w-24 mx-auto tracking-widest'>Skills </h2>
                 <div className='grid grid-cols-2 md:grid-cols-4 gap-12 md:gap-20 px-5 md:px-20 py-10 w-full'>
                     <div className='bg-accent p-8 rounded-3xl HTML'>
                         <img className='w-24 mx-auto flex justify-center items-center' src={html} alt="" />
